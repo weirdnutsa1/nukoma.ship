e17c3b1bf3208857370d40f6980f874cf6f54fc3
Matthias Leckel <63153936+identityiskey@users.noreply.github.com>
2025-03-14 10:23:49 +0000

    newtork new

--- Matthias/Terraform/main.tf
+++ Matthias/Terraform/main.tf
@@ -21,29 +21,7 @@ resource "azurerm_resource_group" "NUKOMASHIP" {
     owner = "matthias.leckel@redbull.com"
   }
 
-}
-# We strongly recommend using the required_providers block to set the
-# Azure Provider source and version being used
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-}
 
-provider "azurerm" {
-  features {}
-}
-
-# Create a resource group
-resource "azurerm_resource_group" "NUKOMASHIP" {
-  name     = "nukoma.ship"
-  location = "West Europe"
-   tags = {
-    owner = "matthias.leckel@redbull.com"
-  }
 }
 resource "azurerm_virtual_network" "hiasnet" {
   name                = "hias-vnet"

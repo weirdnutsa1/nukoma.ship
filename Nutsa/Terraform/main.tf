c31e455104c5fe0cb84b57429199a675500e6e54
weirdnutsa1 <nutsa.tsamalashvili@gmail.com>
2025-03-14 12:17:23 +0000

    Nutsa Network again

--- Nutsa/Terraform/main.tf
+++ Nutsa/Terraform/main.tf
@@ -23,3 +23,16 @@ resource "azurerm_resource_group" "NUKOMASHIP" {
 
 }
 
+resource "azurerm_virtual_network" "hiasnet" {
+  name                = "hias-vnet.Nutsa"
+  address_space       = ["10.252.0.0/16"]
+  location            = azurerm_resource_group.NUKOMASHIP.location
+  resource_group_name = azurerm_resource_group.NUKOMASHIP.name
+}
+
+resource "azurerm_subnet" "subnet1byNutsa" {
+  name                 = "subnet1byNutsa"
+  resource_group_name  = azurerm_resource_group.NUKOMASHIP.name
+  virtual_network_name = azurerm_virtual_network.hiasnet.name
+  address_prefixes     = ["10.252.3.0/24"]
+  }
